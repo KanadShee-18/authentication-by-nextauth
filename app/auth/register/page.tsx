f5d8480a8764452b1680459f3f239d11dc5a63eb
Kanad <codewizardkanad@gmail.com>
2025-01-26 21:05:28 +0530

    Added some styles

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -3,8 +3,12 @@ import React from "react";
 
 const RegisterPage = () => {
   return (
-    <div className="w-full">
-      <RegisterForm />
+    <div className="w-full max-h-[100vh]">
+      <div className="fixed w-3/5 -top-1/4 lg:-top-1/2 aspect-square rounded-full bg-emerald-400 opacity-25 blur-[120px]" />
+      <div className="fixed w-3/5  aspect-square rounded-full -bottom-1/4 lg:-bottom-3/4 -right-10 bg-indigo-400 opacity-25 blur-[120px]" />
+      <div className="relative">
+        <RegisterForm />
+      </div>
     </div>
   );
 };
--- app/auth/reset/page.tsx
+++ app/auth/reset/page.tsx
@@ -3,8 +3,12 @@ import React from "react";
 
 const ResetPassword = () => {
   return (
-    <div className="w-full">
-      <ResetForm />
+    <div className="w-full max-h-[100vh]">
+      <div className="fixed w-3/5 -top-1/4 lg:-top-1/2 aspect-square rounded-full bg-indigo-400 opacity-25 blur-[120px]" />
+      <div className="fixed w-3/5 -bottom-1/4 lg:-bottom-3/4 -right-10 aspect-square rounded-full bg-emerald-400 opacity-25 blur-[120px]" />
+      <div className="relative">
+        <ResetForm />
+      </div>
     </div>
   );
 };
